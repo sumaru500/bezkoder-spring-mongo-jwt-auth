77db0fe07d4e5608d86d7ca9f03b5acfdb97ca29
sumaru500 <phamthang37@yahoo.com>
2022-07-01 12:12:02 +0200

    Init commit project

---
+++ spring-mongo-jwt-auth/src/main/java/jwt/eesolutions/com/SpringMongoJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package jwt.eesolutions.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoJwtAuthApplication.class, args);
+	}
+
+}
---
+++ spring-mongo-jwt-auth/src/test/java/jwt/eesolutions/com/SpringMongoJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package jwt.eesolutions.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
